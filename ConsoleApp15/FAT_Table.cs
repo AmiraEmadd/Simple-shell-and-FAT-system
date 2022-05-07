2432ff3f38432f448d81de68df2b76604d490770
Amira Emad <amiraemad5555@gmail.com>
2022-05-07 22:45:05 +0200

    import

---
+++ ConsoleApp15/FAT_Table.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    public static class FAT_Table
+    {
+        public static int[] FAT = new int[1024];
+        public static void createFAT()
+        {
+            for (int i = 0; i < FAT.Length; i++)
+            {
+                if (i == 0 || i == 4)
+                {
+                    FAT[i] = -1;
+                }
+                else if (i > 0 && i <= 3)
+                {
+                    FAT[i] = i + 1;
+                }
+                else
+                {
+                    FAT[i] = 0;
+                }
+            }
+        }
+        public static void writeFAT()
+        {
+            byte[] FATBYTES = Converter.ToBytes(FAT_Table.FAT);
+            List<byte[]> ls = Converter.SplitBytes(FATBYTES);
+            for (int i = 4; i < ls.Count-1; i++)
+            {
+                Virtual_Disk.write_cluster(ls[i], i + 1, 0, ls[i].Length);
+            }
+        }
+        public static void readFAT()
+        {
+            List<byte> ls = new List<byte>();
+            for (int i = 1; i <= 4; i++)
+            {
+                ls.AddRange(Virtual_Disk.read_cluster(i));
+
+            }
+            FAT = Converter.Toint(ls.ToArray());
+        }
+        public static void printFAT()
+        {
+            Console.WriteLine("FAT HAS THE FOLLOWING: ");
+            for (int i = 0; i < FAT.Length; i++)
+            {
+                Console.WriteLine("FAT [" + i + "] = " + FAT[i]);
+
+            }
+        }
+        public static void setFAT(int[] arr)
+        {
+            if (arr.Length <= 1024)
+                FAT = arr;
+        }
+        public static int getAvalableCluster()
+        {
+            for (int i = 0; i < FAT.Length; i++)
+            {
+                if (FAT[i] == 0)
+                    return i;
+            }
+            return -1;
+        }
+        public static void setClusterPointer(int clusterIndex, int pointer)
+        {
+            FAT[clusterIndex] = pointer;
+        }
+        public static int getClusterPointer(int clusterIndex)
+        {
+            if (clusterIndex >= 0 && clusterIndex < FAT.Length)
+            {
+                return FAT[clusterIndex];
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+}
---
+++ ConsoleApp15/Virtual_Disk .cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+
+
+namespace ConsoleApp15
+{
+    public static class Virtual_Disk
+    {
+        public static FileStream Disk;
+        public static void Create_or_Open_Disk(string path)
+        {
+            Disk = new FileStream(path, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+        }
+        public static int getFreeSpace()
+        {
+            return (1024 * 1024) - (int)Disk.Length;    
+        }
+        public static void initalize(string path)
+        {
+            //try
+            //{
+                if(!File.Exists(path))
+                {
+                    Create_or_Open_Disk(path);
+                    byte[] b = new byte[1024];
+                    for(int i=0;i<b.Length;i++)
+                    {
+                        b[i] = 0;
+                    }
+                    write_cluster(b, 0);
+                    FAT_Table.createFAT();
+                    Directory root = new Directory("H:", 0x10, 5, null);
+                    root.writeDirectory();
+                    FAT_Table.setClusterPointer(5, -1);
+                    Program.current = root;
+                    FAT_Table.writeFAT();
+                }
+                else
+                {
+                    Create_or_Open_Disk(path);
+                    FAT_Table.readFAT();
+                    Directory root = new Directory("H:", 0x10, 5, null);
+                    root.readDirectory();
+                    Program.current = root;
+                }
+            //}catch(IOException ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+        }
+
+       public static void write_cluster(byte[] cluster , int cluster_index , int offset=0 , int count =1024)
+        {
+            Disk.Seek(cluster_index * 1024, SeekOrigin.Begin);
+            Disk.Write(cluster, offset, count);
+            Disk.Flush();
+        }
+
+        public static byte[] read_cluster(int cluster_index)
+        {
+            Disk.Seek(cluster_index * 1024 , SeekOrigin.Begin);
+            byte[] bytes = new byte[1024];
+            Disk.Read(bytes, 0, 1024);
+            return bytes;
+        }
+
+    }
+}
